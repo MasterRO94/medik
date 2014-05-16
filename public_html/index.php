632d97c4900a867734bdfd6f661afd1a396bd802
MasterRO94 <igoshin18@gmail.com>
2014-05-17 00:09:16 +0300

    My First Commit

---
+++ public_html/index.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Медтехник | Медицинский иструмент, шовный материал, лабораторная посуда и не только… </title>
+    <link rel="stylesheet" type="text/css" href="style/main_style.css">
+    
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/main_script.js"></script>
+</head>
+
+<body>
+<div class="wrapper">
+
+    <div class="conteiner">
+
+        <header class="header">
+            <div class="logo_img"><img src="images/logo.gif" alt="logo"/></div>
+            <div class="logo">
+                <h1><a href="#">Медтехник</a></h1>
+                <p>г. Харьков, проспект 50 лет СССР, 56/3,
+                    <br>
+                    т. +38(057)716-41-13, +38(097)979-38-29
+                </p>
+            </div>
+        </header>
+
+        <div class="menu">
+            <nav>
+                <ul>
+                    <li><a href="/">Главная</a></li>
+                    <li><a href="/">Каталог товаров</a></li>
+                    <li><a href="/">О нас</a></li>
+                    <li><a href="/">Контакты</a></li>
+                </ul>
+            </nav>
+            <div class="action"><a href="#">Акции</a></div>
+        </div>
+
+        <div class="main">
+
+            <div class="content">
+
+                <div class="block news">
+                    <h2 class="block_header">Новости</h2>
+                    <ul>
+                        <li>
+                            <p class="date">07 Декабрь 2013</p>
+                            <h2 class="news_title">Новость 3</h2>
+                            <article>This is an example of a WordPress page, you could edit this to put information about yourself or your site so readers know where you are coming from. You can create as many pages like this one or sub-pages as you like and manage all of your content inside of WordPress.</article>
+                        </li>
+                        <li>
+                            <p class="date">07 Декабрь 2013</p>
+                            <h2 class="news_title">Новость 2</h2>
+                            <article>This is an example of a WordPress page, you could edit this to put information about yourself or your site so readers know where you are coming from. You can create as many pages like this one or sub-pages as you like and manage all of your content inside of WordPress.</article>
+                        </li>
+                        <li>
+                            <p class="date">07 Декабрь 2013</p>
+                            <h2 class="news_title">Новость 1</h2>
+                            <article>This is an example of a WordPress page, you could edit this to put information about yourself or your site so readers know where you are coming from. You can create as many pages like this one or sub-pages as you like and manage all of your content inside of WordPress.</article>
+                        </li>
+                    </ul>
+                    <p class="more"><a href="#">Все новости</a></p>
+                    <br class="clear"/>
+                </div>
+
+                <div class="block latest">
+                    <h2 class="block_header">Новые поступления</h2>
+                    <ul>
+                        <li class="item">
+                            <div class="thumb"><img src="images/zazhim_plastichniy_dlya_pishevoda.jpg"></div>
+                            <article class="info">Зажим эластичный для пищевода 280мм З-7-1</article>
+                            <div class="item_footer">
+                                <span class="about"><a href="">Подробнее</a></span>
+                                <span class="buy"><a href="">Купить</a></span>
+                            </div>
+                        </li>
+                        <li class="item">
+                            <div class="thumb"><img src="images/zazhim_plastichniy_dlya_pishevoda.jpg"></div>
+                            <article class="info">Зажим эластичный для пищевода 280мм З-7-1</article>
+                            <div class="item_footer">
+                                <span class="about"><a href="">Подробнее</a></span>
+                                <span class="buy"><a href="">Купить</a></span>
+                            </div>
+                        </li>
+                        <li class="item">
+                            <div class="thumb"><img src="images/zazhim_plastichniy_dlya_pishevoda.jpg"></div>
+                            <article class="info">Зажим эластичный для пищевода 280мм З-7-1</article>
+                            <div class="item_footer">
+                                <span class="about"><a href="">Подробнее</a></span>
+                                <span class="buy"><a href="">Купить</a></span>
+                            </div>
+                        </li>
+                        <li class="item">
+                            <div class="thumb"><img src="images/zazhim_plastichniy_dlya_pishevoda.jpg"></div>
+                            <article class="info">Зажим эластичный для пищевода 280мм З-7-1</article>
+                            <div class="item_footer">
+                                <span class="about"><a href="">Подробнее</a></span>
+                                <span class="buy"><a href="">Купить</a></span>
+                            </div>
+                        </li>
+                        <li class="item">
+                            <div class="thumb"><img src="images/zazhim_plastichniy_dlya_pishevoda.jpg"></div>
+                            <article class="info">Зажим эластичный для пищевода 280мм З-7-1</article>
+                            <div class="item_footer">
+                                <span class="about"><a href="">Подробнее</a></span>
+                                <span class="buy"><a href="">Купить</a></span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div class="navigation">
+                <ul>
+                    <li><a href="#">Разделы каталога</a></li>
+                    <li><a href="#">Акушерство и гинекология</a></li>
+                    <li><a href="#">Дезинфицирующие средства</a></li>
+                    <li><a href="#">Диагностика</a></li>
+                    <li><a href="#">Зажимы</a></li>
+                    <li><a href="#">Инструмент медицинский</a></li>
+                    <li><a href="#">Контрольные материалы</a></li>
+                    <li><a href="#">Костыли трости</a></li>
+                    <li><a href="#">Лаборатория: стекло, реактивы</a></li>
+                    <li><a href="#">Лабораторная посуда пластик</a></li>
+                    <li><a href="#">Лабораторное оборудование</a></li>
+                    <li><a href="#">Мебель</a></li>
+                    <li><a href="#">Медицинский инструмент</a></li>
+                    <li><a href="#">Оборудование для стерилизации</a></li>
+                    <li><a href="#">Общая хирургия</a></li>
+                    <li><a href="#">Перевязка</a></li>
+                    <li><a href="#">Расходный материал</a></li>
+                    <li><a href="#">Реактивы и кислоты</a></li>
+                    <li><a href="#">Термометры Ареометры</a></li>
+                    <li><a href="#">Тест-реагенты и быстрые тесты</a></li>
+                    <li><a href="#">Физиотерапия</a></li>
+                    <li><a href="#">Хозяйственные товары</a></li>
+                    <li><a href="#">Шовный материал</a></li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+
+    <footer class="footer">
+        <p>Copyright &copy; 2014 Медтехник - Медицинский иструмент, шовный материал, лабораторная посуда и не только… .</p>
+    </footer>
+
+</div>
+
+</body>
+</html>
---
+++ public_html/js/main_script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+
+
+
+
+}); //Конец ready
